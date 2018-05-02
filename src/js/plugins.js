2f5a4740ff17730b77d2e49d3ce47a679f17f935
Aamir khan <aamirkhan180@gmail.com>
2018-05-03 03:20:49 +0530

    we do not need jquery to smooth scrollTop

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -30,3 +30,37 @@
         };
     }
 }());
+
+// we do not need jquery to smooth scrollTop
+// usage:
+//     var target = document.querySele..;
+//     var duration = 1024;
+//     jump(target.offsetTop, duration);
+window.jump = function (to, duration) {
+    var element = document.documentElement;
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+        
+    var animate = function(){        
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if(currentTime < duration) {
+            setTimeout(animate, increment);
+        }
+    };
+    animate();
+}
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
