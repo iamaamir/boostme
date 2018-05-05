4f629411e32475f14cbf125cc5b2d2e2c59bad46
Aamir khan <aamirkhan180@gmail.com>
2018-05-05 18:57:36 +0530

    when dom is ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+(function () {
+    var win = window, doc = document;
+
+    // use this if you want to load things after loading everything such as img etc
+    win.addEventListener('load', function () {
+
+    }); // window loaded
+
+    // when dom is ready
+    win.addEventListener("DOMContentLoaded", function () {
+        alert("DOMContentLoaded");
+    }); // DOMContentLoaded
+
+})(window, document);
