a91e947216be351c213835dc2ad4de44ffa5fee8
Aamir khan <aamirkhan180@gmail.com>
2018-05-07 23:03:59 +0530

    removed querySelector pollyfill

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,35 +1,3 @@
-//querySelector polyfill
-(function() {
-    if (!document.querySelectorAll) {
-        console.log("dq polyfill");
-        document.querySelectorAll = function(selectors) {
-            var style = document.createElement('style'),
-                elements = [],
-                element;
-            document.documentElement.firstChild.appendChild(style);
-            document._qsa = [];
-
-            style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
-            window.scrollBy(0, 0);
-            style.parentNode.removeChild(style);
-
-            while (document._qsa.length) {
-                element = document._qsa.shift();
-                element.style.removeAttribute('x-qsa');
-                elements.push(element);
-            }
-            document._qsa = null;
-            return elements;
-        };
-    }
-
-    if (!document.querySelector) {
-        document.querySelector = function(selectors) {
-            var elements = document.querySelectorAll(selectors);
-            return (elements.length) ? elements[0] : null;
-        };
-    }
-}());
 
 // we do not need jquery to smooth scrollTop
 // usage:
