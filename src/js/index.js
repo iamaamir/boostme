a245449905b2b895fa6f831acb34ac925249fade
Aamir khan <aamirkhan180@gmail.com>
2018-05-07 23:26:20 +0530

    when ready is not working out of scope

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,28 @@
 "use strict";
 
-(function () {
-    var win = window, doc = document;
+(function() {
+    var win = window,
+        doc = document;
 
+    win.whenReady = function(fn) {
+        if (document.readyState != 'loading') {
+            fn();
+        } else if (document.addEventListener) {
+            document.addEventListener('DOMContentLoaded', fn);
+        } else {
+            document.attachEvent('onreadystatechange', function() {
+                if (document.readyState != 'loading')
+                    fn();
+            });
+        }
+    }
     // use this if you want to load things after loading everything such as img etc
-    win.addEventListener('load', function () {
+    win.addEventListener('load', function() {
 
     }); // window loaded
 
     // when dom is ready
-    whenReady(function(){
-
+    whenReady(function() {
     });
     // DOMContentLoaded
 
