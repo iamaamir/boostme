59f9b18004ee72a6c5454017bc89847bcca0310b
Aamir khan <aamirkhan180@gmail.com>
2018-05-07 23:32:14 +0530

    toggleClass

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -13,6 +13,25 @@ window.removeClass = function(el, className) {
         el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 }
 
+window.toggleClass = function() {
+    if (el.classList) {
+        el.classList.toggle(className);
+    } else {
+        var classes = el.className.split(' ');
+        var existingIndex = -1;
+        for (var i = classes.length; i--;) {
+            if (classes[i] === className)
+                existingIndex = i;
+        }
+
+        if (existingIndex >= 0)
+            classes.splice(existingIndex, 1);
+        else
+            classes.push(className);
+
+        el.className = classes.join(' ');
+    }
+}
 // we do not need jquery to smooth scrollTop
 // usage:
 //     var target = document.querySele..;
