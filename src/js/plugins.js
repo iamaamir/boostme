a420a1544b5c7fd2702fe59443c3b7d914ff5dfc
Aamir khan <aamirkhan180@gmail.com>
2018-05-08 00:03:09 +0530

    sugar to the XMLHttpRequest

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,6 +1,70 @@
 var _ = function(s) { return document.querySelector(s) }
 var _all = function(s) { return document.querySelectorAll(s) }
 
+
+
+//sugar to the XMLHttpRequest
+//Example: ajax.get('/get.php', {foo: 'bar'}, function() {});
+//Example: ajax.post('/post.php', {foo: 'bar'}, function() {});
+window.ajax = {};
+window.ajax.x = function () {
+    if (typeof XMLHttpRequest !== 'undefined') {
+        return new XMLHttpRequest();
+    }
+    var versions = [
+        "MSXML2.XmlHttp.6.0",
+        "MSXML2.XmlHttp.5.0",
+        "MSXML2.XmlHttp.4.0",
+        "MSXML2.XmlHttp.3.0",
+        "MSXML2.XmlHttp.2.0",
+        "Microsoft.XmlHttp"
+    ];
+ 
+    var xhr;
+    for (var i = 0; i < versions.length; i++) {
+        try {
+            xhr = new ActiveXObject(versions[i]);
+            break;
+        } catch (e) {
+        }
+    }
+    return xhr;
+};
+ 
+window.ajax.send = function (url, callback, method, data, async) {
+    if (async === undefined) {
+        async = true;
+    }
+    var x = window.ajax.x();
+    x.open(method, url, async);
+    x.onreadystatechange = function () {
+        if (x.readyState == 4) {
+            callback(x.responseText)
+        }
+    };
+    if (method == 'POST') {
+        x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    }
+    x.send(data)
+};
+ 
+window.ajax.get = function (url, data, callback, async) {
+    var query = [];
+    for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    }
+    window.ajax.send(url + (query.length ? '?' + query.join('&') : ''), callback, 'GET', null, async)
+};
+ 
+window.ajax.post = function (url, data, callback, async) {
+    var query = [];
+    for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    }
+    window.ajax.send(url, callback, 'POST', query.join('&'), async)
+};
+ 
+
 //add a class to the element
 window.addClass = function(el, className) {
     if (el.classList)
