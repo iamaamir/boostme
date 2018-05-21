3ecc860af9796819eb4bec58c3768082bbb9ee9f
Aamir khan <aamirkhan180@gmail.com>
2018-05-21 13:27:38 +0530

    lets have a smooth scroll smoothly

--- src/js/index.js
+++ src/js/index.js
@@ -4,33 +4,44 @@ plugins.js
 
 "use strict";
 
-(function() {
+(function () {
     var win = window,
         doc = document;
 
-    win.whenReady = function(fn) {
+    win.whenReady = function (fn) {
         if (document.readyState != 'loading') {
             fn();
         } else if (document.addEventListener) {
             document.addEventListener('DOMContentLoaded', fn);
         } else {
-            document.attachEvent('onreadystatechange', function() {
+            document.attachEvent('onreadystatechange', function () {
                 if (document.readyState != 'loading')
                     fn();
             });
         }
     }
     // use this if you want to load things after loading everything such as img etc
-    win.addEventListener('load', function() {
+    win.addEventListener('load', function () {
 
     }); // window loaded
 
     // when dom is ready
-    whenReady(function() {
+    whenReady(function () {
         //hide the pre loader when DOM is loaded
         setTimeout(function () { // using setTimeout just to simulate a little loading effect
             addClass(_('.preloader'), 'hidden');
         }, 500);
+
+        //smooth scroll
+        //any anchor having a class scrollTo will gets smooth scrool effect
+        // ex: <a class="scrollTo" href="#next">next</a>
+        forEachElement('.scrollTo', function (link) {
+            on(link, 'click', function (e) {
+                e.preventDefault();
+                var target = _(e.currentTarget.getAttribute('href'));
+                jump(target.offsetTop, 1024);
+            });
+        });
     });
     // DOMContentLoaded
 
